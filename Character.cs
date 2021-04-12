4431e054eefd1b2084ed689db1e98df9065341e3
Anastasia <krivlenya0@gmail.com>
2021-04-12 09:11:47 +0300

    Добавить инвентарь и заклинания

--- Character.cs
+++ Character.cs
@@ -3,57 +3,119 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Подключить namespace класса
+using игра.artifacts;
 
 //Заменить namespace
-namespace Gaaame2
+namespace игра
 {
+    public enum Status_all
+    {
+        Healthy,
+        Weak,
+        Ill,
+        Poisoned,
+        Paralyzed,
+        Dead
+    }
+
+    public  enum Race_all
+    {
+        Human,
+        Gnome,
+        Elf,
+        Orc,
+        Goblin
+    }
+
+    public enum Gender_all
+    {
+        Male,
+        Female
+    }
     interface IComparable
     {
         int CompareTo(Object obj);
     }
-    class Character: IComparable
+    public class Character: IComparable
     {
-        // Это неизменяемые списки (только для чтения)
-        private readonly IReadOnlyList<string> status_all = new List<string> { "нормальное", "ослаблен", "болен", "парализован", "мёртв" };
-        private readonly IReadOnlyList<string> race_all = new List<string> { "человек", "гном", "эльф", "орк", "гоблин" };
-        private readonly IReadOnlyList<string> gender_all = new List<string> { "мужской", "женский" };
-        private static int ID_Count = 0;
+        private static int ID_next= 0;
+
+        protected int hp;
+        protected int Max_hp;
+        protected int age;
+        protected int xp; 
+        protected Status_all status;
+        protected Dictionary<Artifact, int> inventory = new Dictionary<Artifact, int>();
+        //set'ы для них
+
         public int ID
         {
-            get; private set;
+            get; 
         }
         public int Age
-        {
-            get; set;
+        { 
+            get { return age; }
+            set
+            {
+                if (age < 0)
+                    throw new System.ArgumentException
+                        ("Age < 0");
+                age = value;
+            }
         }
         public int HP
         {
-            get; set;
+            get { return hp; }
+            set
+            {
+                if (value < 0) hp = 0;
+                if (value > Max_hp) hp = Max_HP;
+                else hp = value;
+                CheckHP();
+            }
         }
         public int Max_HP
         {
-            get; set;
-        }
+            get { return Max_hp; }
+            set
+            {
+                if (value < 0)
+                    throw new System.ArgumentException
+                        ("Max hp < 0");
+                Max_hp = value
+                ;
+            }
+        } 
         public int XP
         {
-            get; set;
+            get { return xp; }
+            set
+            {
+                if (value < 0)
+                    throw new System.ArgumentException
+                        ("xp < 0");
+                xp = value;
+            }
+
         }
         public string Name
         {
-            get; private set;
+            get ; 
         }
-        public string Status
+        public Status_all Status
         {
-            get; set;
+            get { return status; } set { status = value; if (Status == Status_all.Dead || Status == Status_all.Paralyzed) {Move_ability = Talk_ability = false; } }
         }
-        public string Race
+        public Race_all Race
         {
-            get; private set;
+            get; 
         }
-        public string Gender
+        public Gender_all Gender
         {
-            get; private set;
+            get;
         }
+
         public bool Talk_ability
         {
             get; set;
@@ -62,17 +124,18 @@ namespace Gaaame2
         {
             get; set;
         }
-        public Character(string _name, int _race, int _gender, int _age)
+        public Character(string _name, Race_all _race, Gender_all _gender, int _age)
         {
             // Заменить(?) идентификатор
-            ID = ID_Count;
-            ID_Count += 1;
-            Name = _name;
-            Status = status_all[0];
+            ID = ID_next;
+            ID_next += 1;
+            if (String.IsNullOrEmpty(_name) || _name.Length < 1) throw new System.ArgumentException ("Wtf the Name");
+            else Name = _name;
+            Status = Status_all.Healthy;
             Talk_ability = true;
             Move_ability = true;
-            Race = race_all[_race];
-            Gender = gender_all[_gender];
+            Race = _race;
+            Gender = _gender;
             Age = _age;
             Max_HP = 100;
             HP = Max_HP;
@@ -87,15 +150,15 @@ namespace Gaaame2
                 return 1;
             return 0;
         }
-        public void CheckHP()
+        public virtual void CheckHP()
         {
-            double HP_percent = 100 / (Max_HP / HP);
-            if (HP_percent < 10 && Status == status_all[0])
-                Status = status_all[1];
-            if (HP_percent > 10 && Status == status_all[1])
-                Status = status_all[0];
+            decimal HP_percent = 100 / (Max_HP / HP);
+            if (HP_percent < 10 && Status == Status_all.Healthy)
+                Status = Status_all.Weak;
+            if (HP_percent > 10 && Status == Status_all.Weak)
+                Status = Status_all.Healthy;
             if (HP_percent == 0)
-                Status = status_all[4];
+                Status = Status_all.Dead;
         }
         public override string ToString()
         {
@@ -108,9 +171,55 @@ namespace Gaaame2
                 move_ans = "есть";
             else
                 move_ans = "нет";
-            return $"**********\nID: {ID}\nИмя: {Name}\nСостояние: {Status}\nВозможность разговаривать: {talk_ans}\n" +
+            return $"ID: {ID}\nИмя: {Name}\nСостояние: {Status}\nВозможность разговаривать: {talk_ans}\n" +
                 $"Возможность двигаться: {move_ans}\nРаса: {Race}\nПол: {Gender}\nВозраст: {Age}\nЗдоровье: {HP}\n" +
-                $"Максимальное здоровье: {Max_HP}\nОпыт: {XP}\n**********";
+                $"Максимальное здоровье: {Max_HP}\nОпыт: {XP}";
+        }
+        public void ArtifactCheck(Artifact artifact)
+        {
+            if (!inventory.ContainsKey(artifact))
+                throw new NullReferenceException("The artifact is not in the inventory.");
+        }
+        public void GetArtifact(Artifact artifact)
+        {
+            if (inventory.ContainsKey(artifact))
+                ++inventory[artifact];
+            else
+                inventory.Add(artifact, 1);
+        }
+        public void ThrowArtifact(Artifact artifact)
+        {
+            ArtifactCheck(artifact);
+            --inventory[artifact];
+            if (inventory[artifact] == 0)
+                inventory.Remove(artifact);
+        }
+        public void GiveArtifact(Artifact artifact, Character character)
+        {
+            ThrowArtifact(artifact);
+            character.GetArtifact(artifact);
+        }
+        public void UseArtifact(Artifact artifact, Character character)
+        {
+            ArtifactCheck(artifact);
+            artifact.Perform_a_magic_effect(character);
+            if (!artifact.CanUse)
+                ThrowArtifact(artifact);
+        }
+        public void UseArtifact(Artifact artifact, Character character, int power)
+        {
+            ArtifactCheck(artifact);
+            artifact.Perform_a_magic_effect(character, power);
+            if (!artifact.CanUse)
+                ThrowArtifact(artifact);
+        }
+        static void Main(string[] args)
+        {
+            var abc = new MagicCharacter("Xa", Race_all.Elf, Gender_all.Female, 15);
+            abc.XP = 150;
+            Console.WriteLine(abc.ToString());
+            Console.ReadKey();
         }
     }
+
 }
---
+++ MagicCharacter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Подключить namespace класса
+using RPG;
+
+namespace игра
+{
+
+    class MagicCharacter : Character
+    {
+        protected int mp;
+        protected int Max_mp;
+        protected Dictionary<Spell, bool> spells = new Dictionary<Spell, bool>();
+        //public List<Spells> spells = new List<Spells>;  
+        // или enum
+
+        public int Max_MP
+        {
+            get { return Max_mp; }
+            set
+            {
+                if (value < 0)
+                    throw new System.ArgumentException
+                        ("Max MP < 0");
+                Max_mp = value
+                ;
+            }
+        }
+        public int MP
+        {
+            get { return mp; }
+            set { mp = value; if (mp < 0) mp = 0; if (mp > Max_mp) mp = Max_mp; }
+        }
+        //public MagicCharacter(string _name, int _race, int _gender, int _age) : base(_name, _race, _gender, _age)
+        //{
+        //    Max_mp = 200;
+        //    mp = Max_mp;
+        //}
+        public MagicCharacter(string _name, Race_all _race, Gender_all _gender, int _age) : base(_name, _race, _gender, _age)
+        {
+            Max_mp = 200;
+            mp = Max_mp;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + "\n"
+                + $"Mana: {MP}/{Max_MP}";
+        }
+        public void CheckSpell(Spell spell)
+        {
+            if (!spells.ContainsKey(spell))
+                throw new NullReferenceException("The spell is not learned.");
+        }
+        public void LearnSpell(Spell spell)
+        {
+            spells[spell] = true;
+        }
+        public void ForgetSpell(Spell spell)
+        {
+            CheckSpell(spell);
+            spells.Remove(spell);
+        }
+        public void UseSpell(Spell spell, Character character)
+        {
+            CheckSpell(spell);
+            spell.Perform_a_magic_effect(character);
+        }
+        public void UseSpell(Spell spell, Character character, int power)
+        {
+            CheckSpell(spell);
+            spell.Perform_a_magic_effect(character, power);
+        }
+    }
+}

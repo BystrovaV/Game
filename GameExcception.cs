86a404d1bb8b3a087f0c428244176304dd42c74f
BystrovaV <veronikabystrova94@gmail.com>
2021-04-30 14:29:30 +0300

    Add gameexception to main

---
+++ GameExcception.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    public class GameException : Exception
+    {
+        public GameException() : base("Game problem") { }
+        public GameException(string message) : base(message) { }
+    }
+}
